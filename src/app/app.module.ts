e9f2747f011c794719ee7b1347473d0d0441161a
preethikumar <preethikumar@benfy.co>
2019-06-12 14:10:04 +0530

    added -home slider -footer -navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SliderComponent } from './components/slider/slider.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavComponent,
+    ContactComponent,
+    FooterComponent,
+    AboutComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
